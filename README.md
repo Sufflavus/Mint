8896dce74e6a7f40bb4485e040876768287cc3de
Tania Shatilova <tatashat@gmail.com>
2015-11-03 21:08:03 +0300

    * Colibri.utils moved to Mint.utils

--- README.md
+++ README.md
@@ -1,2 +1,198 @@
 # Mint
-JavaScript utils
+
+JavaScript helpful functions
+
+## Setup & Dependencies
+1. mint-utils.js (does not have any dependencies)
+
+```html
+<head>
+    
+</head>
+<body>
+
+    <script src="../src/mint-utils.js"></script>
+</body>
+```
+
+1. Mint.utils.createGuid()
+1. Mint.utils.getRandomInt(min, max)
+1. Mint.utils.isArray(obj)
+1. Mint.utils.isFunction(obj)
+1. Mint.utils.arrayFirst(array, predicate)
+1. Mint.utils.arrayRemoveItem(array, itemToRemove)
+1. Mint.utils.dictionaryForEach(dictionary, callback)
+
+## Mint.utils functions 
+### 1. Mint.utils.createGuid()
+> The **createGuid()** method generates new random Guid.
+
+#### Syntax
+```js
+var guid = Mint.utils.createGuid();
+```
+
+#### Return Value
+New random **Guid** (string).
+
+#### Examples
+
+```js
+Mint.utils.createGuid();  // "0ef73963-bea8-ebf9-af71-897958421e2e"
+```
+
+### 2. Mint.utils.getRandomInt()
+> The **getRandomInt()** method returns a random number within a specified range.
+
+#### Syntax
+```js
+var random = Mint.utils.getRandomInt([min[, max]]);
+```
+
+#### Parameters
+* **min** - (int, optional) The inclusive lower bound of the random number returned. Default value is 0.
+* **max** - (int, optional) The exclusive upper bound of the random number returned. Max must be greater than or equal to min. Default value is Number.MAX_VALUE.
+
+#### Return Value
+A signed integer greater than or equal to min and less or equal than max; that is, the range of return values includes min and max. If min equals max, min is returned. 
+
+#### Examples
+
+```js
+Mint.utils.getRandomInt(1, 8);  // 6
+Mint.utils.getRandomInt(1);     // 1.794066025907234e+308
+Mint.utils.getRandomInt();      // 1.0024528559121937e+308
+Mint.utils.getRandomInt("12");  // Error
+
+```
+
+### 3. Mint.utils.isArray()
+> The **isArray()** method determines whether the tested object is Array.
+
+#### Syntax
+```js
+var isArray = Mint.utils.isArray([]);
+```
+
+#### Parameters
+* **obj** - (object) An object for check.
+
+#### Return Value
+**true** if the tested object is Array; otherwise, **false**.
+
+#### Examples
+
+```js
+Mint.utils.isArray([1,2,3]);      // true
+Mint.utils.isArray(new array());  // true
+Mint.utils.isArray("a");          // false
+Mint.utils.isArray(null);         // false
+```
+
+### 4. Mint.utils.isFunction()
+> The **isFunction()** method determines whether the tested object is Function.
+
+#### Syntax
+```js
+var testedObject = new function(){};
+var isFunction = Mint.utils.isFunction(testedObject);
+```
+
+#### Parameters
+* **obj** - (object) An object for check.
+
+#### Return Value
+**true** if the tested object is Function; otherwise, **false**.
+
+#### Examples
+
+```js
+function test(){ };
+Mint.utils.isFunction(test);       // true
+Mint.utils.isFunction("a");        // false
+Mint.utils.isFunction(null);       // false
+Mint.utils.isFunction([1, 2, 3]);  // false
+```
+
+### 5. Mint.utils.arrayFirst()
+> The **arrayFirst()** method returns the first item of an array that is matched the predicate function.
+
+#### Syntax
+```js
+avar first = Mint.utils.arrayFirst(array, predicate);
+```
+
+#### Parameters
+* **array** - array in which item is searching.
+* **predicate** - (function) function that returns true for searching item.
+
+#### Return Value
+The first item of an array that is matched the predicate function; null if item is not found.
+
+#### Examples
+
+```js
+
+var array = [1, 3, 8];          
+var first = Mint.utils.arrayFirst(array, function (item){
+    return item > 2;
+});
+// first equal 3
+
+first = Mint.utils.arrayFirst(array, function (item){
+    return item > 10;
+});
+// first is null
+
+```
+
+### 4. Mint.utils.arrayRemoveItem()
+> The **arrayRemoveItem()** method removes first accurance of item from an array.
+> The arrayRemoveItem() method removes item from array that is passed as first parameter.
+
+#### Syntax
+```js
+Mint.utils.arrayRemoveItem(array, itemToRemove);
+```
+
+#### Parameters
+* **array** - array in which item is searching.
+* **itemToRemove** - (object) item of array that should be removed.
+
+#### Examples
+
+```js
+var array = [1, 3, 8];      
+Mint.utils.arrayRemoveItem(array, 3); // array == [1, 8]
+```
+
+### 5. Mint.utils.dictionaryForEach()
+> The **dictionaryForEach()** method executes a provided function once per dictionary element.
+> The dictionaryForEach() method executes only for own properties of an object.
+
+#### Parameters
+* **dictionary** - object that structured like a dictionary, for example, {a: 1, b: 2}.
+* **callback** - function to execute for each element, taking two arguments: 
+  * **key** - the current element's key being processed in the dictionary, and 
+  * **value** - the current element's value.
+
+#### Examples
+
+```js
+var dict = {a: 1, b: 2};
+
+function logDictionaryElements(key, value) {
+  console.log("key = " + key + "; value = " + value);
+}
+
+Mint.utils.dictionaryForEach(dict, logDictionaryElements);
+
+// logs:
+// key = a; value = 1
+// key = b; value = 2
+
+
+```
+
+## LICENSE
+[The MIT License](https://github.com/Sufflavus/Mint/blob/master/LICENSE)
--- src/mint-utils.js
+++ src/mint-utils.js
@@ -3,121 +3,131 @@
 // @version 1.1.0 
 
 var Mint = {};
-Mint.utils = (function(){
-  
-  /**
-   * Generates random Guid (has tests)
-   * @return {String} random Guid. Example: "656fa0e4-11e9-4b89-8ae5-752cfe807b8f".
-   * https://en.wikipedia.org/wiki/Globally_unique_identifier
-   */  
-  function createGuid() {     
-      var s4 = generateSymbolsQuartet; 
-      return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-             s4() + '-' + s4() + s4() + s4();      
-  }
-
-  /**
-   * Generates random integer (has tests)
-   * @param {Number} min 
-   * @param {Number} max
-   * @return {Number} random integer that is between min and max. 
-   * If min is not specified it set to 0. If max is not specified it set to Number.MAX_VALUE.
-   */
-  function getRandomInt(min, max) { 
-      if(min && !Number.isInteger(min)){
-        throw new Error("If the first parameter specified it should be int.");
-      }
-
-      if(max && !Number.isInteger(max)){
-        throw new Error("If the second parameter specified it should be int.");
-      }
-
-      min = Number.isInteger(min) ? min : 0;
-      max = Number.isInteger(max) ? max : Number.MAX_VALUE;
-
-      if(min > max){
-        throw new Error("First parameter should be less or equal then the second one.");
-      }
-
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  /**
-   * Returns the first item of an array that matched the predicate function (has tests)
-   * @param {Array} array 
-   * @param {function} predicate
-   * @return {object} the first item of an array that matched the predicate function. 
-   */
-  function arrayFirst(array, predicate) {
-    if(!array || !array.isArray()){
-      throw new Error("First parameter should be array.");
+Mint.utils = (function() {
+    
+    /**
+     * Generates random Guid (has tests)
+     * @return {String} random Guid. Example: "656fa0e4-11e9-4b89-8ae5-752cfe807b8f".
+     * https://en.wikipedia.org/wiki/Globally_unique_identifier
+     */  
+    function createGuid() {     
+        var s4 = generateSymbolsQuartet; 
+        return s4() + s4() + "-" + s4() + "-" + s4() + "-" +
+             s4() + "-" + s4() + s4() + s4();      
     }
 
-    if(!predicate || !predicate.isFunction()){
-      throw new Error("Second parameter should be a function.");
-    }
-
-    for (var i = 0, j = array.length; i < j; i++){
-      if (predicate(array[i])) {
-          return array[i];
+    /**
+     * Generates random integer (has tests)
+     * @param {Number} min 
+     * @param {Number} max
+     * @return {Number} random integer that is between min and max. 
+     * If min is not specified it set to 0. If max is not specified it set to Number.MAX_VALUE.
+     */
+    function getRandomInt(min, max) { 
+        if(min && !Number.isInteger(min)) {
+            throw new Error("If the first parameter specified it should be int.");
         }
-      }
 
-    return null;
-  }
+        if(max && !Number.isInteger(max)) {
+            throw new Error("If the second parameter specified it should be int.");
+        }
 
-  /**
-   * Removes first accurance of item from an array. (has tests)
-   * @param {Array} array 
-   * @param {object} itemToRemove 
-   */
-  function arrayRemoveItem(array, itemToRemove) {
-    if(!array || !array.isArray()){
-      throw new Error("First parameter should be array.");
+        min = Number.isInteger(min) ? min : 0;
+        max = Number.isInteger(max) ? max : Number.MAX_VALUE;
+
+        if(min > max){
+            throw new Error("First parameter should be less or equal then the second one.");
+        }
+
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    var index = array.indexOf(itemToRemove);
-    if (index >= 0) {
-      array.splice(index, 1);
-    }    
-  }   
-
-  /**
-   * Executes a provided function once per dictionary element. (has tests)
-   * @param {Object} dictionary. Example: dicr = {a: 1, b: 2}
-   * @param {function} callback 
-   */
-  function dictionaryForEach(dictionary, callback){
-    if (!dictionary) {
-      throw new Error("First parameter should be defined.");
+    function isArray(obj) {
+        return Object.prototype.toString.call(obj) === "[object Array]";
     }
 
-    if(!callback || !callback.isFunction()){
-      throw new Error("Second parameter should be a function.");
+    function isFunction(obj) {
+        return Object.prototype.toString.call(obj) === "[object Function]";
     }
 
-    for (var key in dictionary) {
-      if (!dictionary.hasOwnProperty(key)) { 
-        continue; 
-      }
+    /**
+     * Returns the first item of an array that matched the predicate function (has tests)
+     * @param {Array} array 
+     * @param {function} predicate
+     * @return {object} the first item of an array that matched the predicate function. 
+     */
+    function arrayFirst(array, predicate) {
+        if(!array || !isArray(array)){
+            throw new Error("First parameter should be array.");
+        }
 
-      var value = dictionary[key];
-      callback(key, value);
+        if(!predicate || !isFunction(predicate)) {
+            throw new Error("Second parameter should be a function.");
+        }
+
+        for (var i = 0, j = array.length; i < j; i++){
+            if (predicate(array[i])) {
+                return array[i];
+            }
+        }
+
+        return null;
     }
-  }
 
-  function generateSymbolsQuartet() {
-      return Math.floor((1 + Math.random()) * 0x10000)
+    /**
+     * Removes first accurance of item from an array. (has tests)
+     * @param {Array} array 
+     * @param {object} itemToRemove 
+     */
+    function arrayRemoveItem(array, itemToRemove) {
+        if(!array || !isArray(array)){
+            throw new Error("First parameter should be array.");
+        }
+
+        var index = array.indexOf(itemToRemove);
+        if (index >= 0) {
+            array.splice(index, 1);
+        }    
+    }   
+
+    /**
+     * Executes a provided function once per dictionary element. (has tests)
+     * @param {Object} dictionary. Example: dicr = {a: 1, b: 2}
+     * @param {function} callback 
+     */
+    function dictionaryForEach(dictionary, callback) {
+        if (!dictionary) {
+            throw new Error("First parameter should be defined.");
+        }
+
+        if(!callback || !isFunction(callback)) {
+            throw new Error("Second parameter should be a function.");
+        }
+
+        for (var key in dictionary) {
+            if (!dictionary.hasOwnProperty(key)) { 
+                continue; 
+            }
+
+            var value = dictionary[key];
+            callback(key, value);
+        }
+    }
+
+    function generateSymbolsQuartet() {
+        return Math.floor((1 + Math.random()) * 0x10000)
                  .toString(16)
                  .substring(1);
-  }
+    }
 
-  return {
-    createGuid: createGuid,
-    getRandomInt: getRandomInt,
-    arrayFirst: arrayFirst,
-    arrayRemoveItem: arrayRemoveItem,
-    dictionaryForEach: dictionaryForEach
-  }
+    return {
+        createGuid: createGuid,
+        getRandomInt: getRandomInt,
+        isArray: isArray,
+        isFunction: isFunction,
+        arrayFirst: arrayFirst,
+        arrayRemoveItem: arrayRemoveItem,
+        dictionaryForEach: dictionaryForEach
+    }
 })();
 
